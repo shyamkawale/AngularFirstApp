9eded4b1174ed86f43fc03fec8c8f728aa727db6
Shyam Kawale <shyamvilaskawale@gmail.com>
2023-06-17 16:57:52 +0530

    removed unnecessary code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
